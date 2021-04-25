d3479665159e26c0c4c31dd4f6944c2af552cdde
mmaarouf <maarouf.9@gmail.com>
2021-04-25 20:03:20 +1000

    Move lua module code into a web worker

--- web/index.js
+++ web/index.js
@@ -1,9 +1,1 @@
-var Module = {
-  print: text => console.log(text),
-  printErr: text => console.error(text),
-  onRuntimeInitialized: () => console.log("ready"),
-};
-
-const runLua = (code) => {
-  return Module.ccall("runLua", 'string', ['string'], [code]);
-}
+const luaInteropWorker = new Worker("lua-interop-worker.js");
---
+++ web/lua-interop-worker.js
@@ -0,0 +1,17 @@
+/**
+ * The lua code dynamically loads up the lua-cjson library at which point
+ * Chromea complains that loading up a library that is greater then 4kb is not allowed on the
+ * main thread as it blocks it.
+ * Therefore using web workers to load and execute the wasm module functions.
+ */
+var Module = {
+  print: text => console.log(text),
+  printErr: text => console.error(text),
+  onRuntimeInitialized: () => console.log("ready"),
+};
+
+const runLua = (code) => {
+  return Module.ccall("runLua", 'string', ['string'], [code]);
+}
+
+self.importScripts('lua-interop.js')
