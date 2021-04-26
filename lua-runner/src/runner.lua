179773f491a0174ff2501c4c4db020a68a8f51be
mmaarouf <maarouf.9@gmail.com>
2021-04-26 19:48:50 +1000

    Add ability to return error and success responses

--- lua-runner/src/runner.lua
+++ lua-runner/src/runner.lua
@@ -1,7 +1,32 @@
+local cjson = require 'cjson.safe'
+
 local runner = {}
 
+local function create_ok_response(data)
+    return {
+        result = 'ok',
+        data = data
+    }
+end
+
+local function create_error_response(data)
+    return {
+        result = 'error',
+        data = data
+    }
+end
+
 function runner.run(fn_to_run)
-    return '{"result":"ok","data":"hello"}'
+    local res, data = pcall(fn_to_run)
+
+    local response_data
+    if res then
+        response_data = create_ok_response(data)
+    else
+        response_data = create_error_response(data)
+    end
+
+    return cjson.encode(response_data)
 end
 
 return runner
--- lua-runner/test/runner_spec.lua
+++ lua-runner/test/runner_spec.lua
@@ -1,12 +1,23 @@
 local runner = require 'runner'
 
 describe('runner', function()
+
     it('should return successful result of function in json', function()
         local fn_to_run = function() return 'hello' end
 
         local actual = runner.run(fn_to_run)
 
         local expected = '{"result":"ok","data":"hello"}'
+        assert.equals(expected, actual)
+    end)
+
+    it('should return unseccessful result with error message when error occurs', function()
+        local no_error_position_level = 0
+        local fn_to_run = function() error('error message', no_error_position_level) end
+
+        local actual = runner.run(fn_to_run)
+
+        local expected = '{"result":"error","data":"error message"}'
         assert.equals(expected, actual)
     end)
 end)
